3d29591b401da22f67eb62317160ea263c54aed7
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2017-04-13 10:14:31 +0200

    import contracts

--- src/heard_beat/contracts.cljs
+++ src/heard_beat/contracts.cljs
@@ -2,3 +2,5 @@
   (:require-macros [heard-beat.truffle.macros :refer [defcontract]]))
 
 (defcontract coin "src/truffle/build/contracts/MetaCoin.json")
+(defcontract heart-beat "src/truffle/build/contracts/HeartBeat.json")
+(defcontract counter "src/truffle/build/contracts/Counter.json")
