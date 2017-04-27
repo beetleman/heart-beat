a70134d6595fb330d75d243ff677a19aaa2698b1
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2017-04-27 02:14:25 +0200

    remove unused contracts

--- src/truffle/migrations/2_deploy_contracts.js
+++ src/truffle/migrations/2_deploy_contracts.js
@@ -1,14 +1,7 @@
-const ConvertLib = artifacts.require("./ConvertLib.sol");
-const MetaCoin = artifacts.require("./MetaCoin.sol");
-
 const HeartBeat = artifacts.require("./HeartBeat.sol");
 const Counter = artifacts.require("./Counter.sol");
 
 module.exports = async function(deployer, network, accounts) {
-    deployer.deploy(ConvertLib);
-    deployer.link(ConvertLib, MetaCoin);
-    deployer.deploy(MetaCoin);
-
     let counter;
     let heatBeat;
 
